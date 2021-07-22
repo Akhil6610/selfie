b4e03bb165fd1c5e5a8ddff7ec96f80accc2a566
Akhil6610 <82265419+Akhil6610@users.noreply.github.com>
2021-07-22 20:57:35 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,45 @@
+var SpeechRecognition=window.webkitSpeechRecognition;
+var recognition=new SpeechRecognition
+function start(){
+document.getElementById("textbox").innerHTML="";
+recognition.start();
+}
+recognition.onresult=function run(event){
+    console.log(event);
+var content=event.results[0][0].transcript;
+document.getElementById("textbox").innerHTML=content;
+if(content=="Take my selfie"){
+    console.log("taking selfie");
+speak();
+}
+}
+function speak(){
+var synth=window.speechSynthesis;
+var speakdata="taking your selfie in 5 Seconds";
+var speakthis=new SpeechSynthesisUtterance(speakdata);
+synth.speak(speakthis);
+Webcam.attach(camera);
+setTimeout(function(){
+    take_image();
+    save();
+}, 5000);
+
+}
+camera = document.getElementById("camera");
+Webcam.set({
+    width:900,
+    height:300,
+    image_format : 'jpeg',
+    jpeg_quality:90
+});
+function take_image(){
+    Webcam.snap(function (data_uri){
+        document.getElementById("result").innerHTML='<img id="selfie_image" src="'+data_uri+'">';
+    });
+}
+function save(){
+link=document.getElementById("link");
+image=document.getElementById("selfie_image").src;
+link.href=image;
+link.click();
+}
